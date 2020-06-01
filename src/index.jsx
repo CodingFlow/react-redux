58befb766c83ba8db07fbd0ccf4875ca9ece8671
DarknessFlames <blueflamesstrike@hotmail.com>
2020-05-31 21:10:31 -0400

    Add redux-thunk and redux-logger packages and set them up.

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,21 @@
+import thunkMiddleware from 'redux-thunk'
+import { createLogger } from 'redux-logger'
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import rootReducer from './reducers'
 import App from './App'
 
-const store = createStore(rootReducer)
+const loggerMiddleware = createLogger()
+
+const store = createStore(
+  rootReducer,
+  applyMiddleware(
+    thunkMiddleware,
+    loggerMiddleware
+  )
+)
 
 render(
   <Provider store={store}>
